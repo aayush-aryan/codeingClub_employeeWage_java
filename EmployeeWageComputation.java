f38a36cc9e7d343275905672a9e82b70b01e1d13
unknown <log2aayusharyan@gmail.com>
2020-09-15 10:46:49 -0700

    welcome message for employeeWageComputation

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package org.bridgelabz.employeewagecomputation;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to employeeWageComputation ");
+
+	}
+
+}
