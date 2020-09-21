ce543bc846aa7c649e228336efcb2bd600e9a5c3
aayush-aryan <log2aayusharyan@gmail.com>
2020-09-21 14:01:55 +0530

    calculating employeeWage using method

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,29 +1,57 @@
 package org.bridgelabz.employeewagecomputation;
 
 public class EmployeeWageComputation {
-    static int wagePerHour=20;
-	static int isPresent=1;
-	static int isPartTime = 2;
-	static int empHrs=0;
-	static int empWage=0;
+	
+	  static int wagePerHour=20;
+		static int isPresent=1;
+		static int isPartTime = 2;
+		static int totalWorkingDays=20;
+		static int totalWorkingHrs=100;
+		static int empHrs;
+		static int empDay=0;
+		static int empWage;
+		static int totalEmpHrs=0;
 
-	public static void main(String[] args) {
-	     
-		  double randomValue=Math.floor(Math.random()*10)%3;
-			if (randomValue == isPresent)
-			{
-				empHrs=8;
-			}
-			else if(randomValue == isPartTime)
-			{
-				empHrs=4;
-			}
-			else {
-				empHrs=0;
-			}
-			empWage=empHrs*wagePerHour;
-			System.out.println("Employee wage is :"+ empWage);
+		  
 
-	}
+		public static void computeEmpWage(){
+			
+		
+		   while (empDay < totalWorkingDays && totalEmpHrs< totalWorkingHrs) {
+					
+			  double randomValue=Math.floor(Math.random()*10)%3;
+				switch ((int)randomValue) {
+				
+				case 1:
+					empHrs=8;
+					if(totalEmpHrs<=92) {
+					totalEmpHrs=totalEmpHrs+empHrs;
+					empDay++;
+					}
+					
+					break;
+				case 2:
+					empHrs=4;
+					if(totalEmpHrs<=96) {
+					totalEmpHrs=totalEmpHrs+empHrs;
+					empDay++;
+					}
+					break;
+				default:
+					empHrs=0;
+				}
+		   				
+		   }
+		    
+				empWage=totalEmpHrs*wagePerHour;
+				System.out.println("Total working hours :" +totalEmpHrs+"hrs");
+				System.out.println("Total working days :" +empDay);
+				System.out.println("Total wages of employee :"+ empWage);
+		}
+				
+				public static void main(String[] args) {
+					
+					computeEmpWage();
+		}	
 
 }
